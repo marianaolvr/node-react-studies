dd88239253be89dcf58c8e4251ae22a58b83d19e
Mariana Oliveira <olvr.mariana@gmail.com>
2020-11-25 10:18:25 -0300

    add nodemon

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.json({ message: 'hello world again' });
+    return response.json({ message: 'hello world again outra vez' });
 })
 
-app.listen(3333);
+app.listen(3333, () =>{
+    console.log('Rodando o back, fela 😎️')
+});
