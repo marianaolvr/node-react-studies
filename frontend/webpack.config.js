165aba2769b62ae97821cebb268490779e95d73c
Mariana Oliveira <olvr.mariana@gmail.com>
2020-12-15 20:31:34 -0300

    webpack configuration and webpack-dev-server

---
+++ frontend/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    // mesma coisa que o './src/index.js', mas evitando possíveis conflitos do ambiente Windows, que tem a barra invertida \
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+    }, 
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+};
